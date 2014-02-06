fc8504ac2110a7939d1f1617563ff309aa9cad78
Adam Enger <adamenger@gmail.com>
2014-02-06 17:56:21 -0600

    adding domain support for nginx configurable via attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,6 +2,9 @@
 default[:pypi][:username] = 'pypi'
 default[:pypi][:groupname] = 'pypi'
 
+# which domain to serve requests for e.g: pypi.threadless.com
+default[:pypi][:domain] = 'localhost'
+
 # install dependencies into node[:pypi][:install_directory]
 default[:pypi][:install_directory] = '/mnt/pypi'
 
