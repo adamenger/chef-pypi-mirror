1af1183d669a71255c1e58418212f1d1f5cf6876
Adam Enger <adamenger@gmail.com>
2014-02-09 00:19:20 -0600

    bumping to 1.0.0

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,8 @@ maintainer_email 'adamenger@gmail.com'
 license          'BSD'
 description      'Installs/Configures pypi mirror'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.1'
+version          '1.0.0'
 
 depends 'python'
 depends 'nginx'
+depends 'apt'
